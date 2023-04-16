e1ae9c6dbf46127061efd12184696c24109155fc
FedeCasper <federicorouyere@gmail.com>
2023-04-15 21:49:37 -0300

    Canvas parte 4

---
+++ creeper.js
@@ -0,0 +1,20 @@
+let pizarra = document.querySelector('canvas')
+let pincel = pizarra.getContext('2d')
+
+pincel.fillStyle = "darkgreen"
+pincel.fillRect(0,0,350,300)
+
+pincel.fillStyle = "black"
+pincel.fillRect(50,50,90,90)
+
+pincel.fillStyle = "black"
+pincel.fillRect(210,50,90,90)
+
+pincel.fillStyle = "black"
+pincel.fillRect(140,140,70,100)
+
+pincel.fillStyle = "black"
+pincel.fillRect(100,190,40,110)
+
+pincel.fillStyle = "black"
+pincel.fillRect(210,190,40,110)
