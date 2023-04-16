10d1309c3cfef53744603a96005347f7d064308f
FedeCasper <federicorouyere@gmail.com>
2023-04-15 21:18:54 -0300

    canvas parte2

--- canvas.js
+++ canvas.js
@@ -1,2 +1,16 @@
 const pantalla = document.querySelector('canvas');
 let pincel = pantalla.getContext('2d');
+pincel.fillStyle = "lightgrey"
+pincel.fillRect(0,0, 600, 800)
+
+pincel.fillStyle = "white"
+pincel.fillRect(0,0, 800, 600)
+
+pincel.fillStyle = "dodgerblue"
+pincel.fillRect(0,0, 800, 200)
+
+pincel.fillStyle = "dodgerblue"
+pincel.fillRect(0,400, 800, 200)
+
+
+
