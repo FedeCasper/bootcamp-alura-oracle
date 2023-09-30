cb022d138df6e4e520d47a56cf00a47aa71e09e9
FedeCasper <federicorouyere@gmail.com>
2023-09-30 16:49:42 -0300

    Maven Project

---
+++ base-de-datos/1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -0,0 +1,69 @@
+package com.alura.jdbc.controller;
+
+import com.alura.jdbc.factory.ConnectionFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductoController {
+
+	public void modificar(String nombre, String descripcion, Integer id) {
+		// TODO
+	}
+
+	public int eliminar(Integer id) throws SQLException {
+		Connection con = new ConnectionFactory().recuperaConexion();
+
+		Statement statement = con.createStatement();
+
+		statement.execute("DELETE FROM PRODUCTO WHERE ID = " + id);
+
+		return statement.getUpdateCount();
+	}
+
+	public List<Map<String, String>> listar() throws SQLException {
+
+		Connection con = new ConnectionFactory().recuperaConexion();
+
+		Statement statement = con.createStatement();
+
+		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+		ResultSet resultSet = statement.getResultSet();
+
+		List<Map<String, String>> resultado = new ArrayList<>();
+
+		while (resultSet.next()){
+			Map<String, String> fila = new HashMap<>();
+			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+			fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+			fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+			resultado.add(fila);
+		}
+
+		con.close();
+
+		return resultado;
+	}
+
+    public void guardar(Map<String, String> producto) throws SQLException {
+		Connection con = new ConnectionFactory().recuperaConexion();
+		Statement statement = con.createStatement();
+		statement.execute("INSERT INTO PRODUCTO (nombre, descripcion, cantidad)"
+				+ "VALUES('" + producto.get("NOMBRE") + "', '"
+				+ producto.get("DESCRIPCION") + "', "
+				+ producto.get("CANTIDAD") + ")", Statement.RETURN_GENERATED_KEYS);
+		ResultSet resultSet = statement.getGeneratedKeys();
+
+		while(resultSet.next()){
+			System.out.println("Fue insertado el producto de id" + resultSet.getInt(1));
+			resultSet.getInt(1);
+		}
+	}
+
+}
