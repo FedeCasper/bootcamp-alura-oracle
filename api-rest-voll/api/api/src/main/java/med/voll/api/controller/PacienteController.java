1f716fae071c10fd0c46d8d64039639c2c3e6c9d
FedeCasper <federicorouyere@gmail.com>
2023-10-01 21:39:08 -0300

    Spring Security

--- api-rest-voll/api/api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api-rest-voll/api/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.domain.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- api-rest-voll/api/api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
+++ api-rest-voll/api/api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -1,6 +1,6 @@
 package med.voll.api.paciente;
 
-import med.voll.api.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosRegistroPaciente(String nombre, String email, String telefono, String documento, DatosDireccion datosDireccion) {
 }
